07d19bc959ead0060df623dda8171036ace83bc1
Carolynn Vu <carolynn.vu@gmail.com>
2017-09-21 00:45:26 -0400

    create helper method

--- tail.c
+++ tail.c
@@ -4,10 +4,44 @@
 
 char buf[4096];
 
+
+void tail(int fd, char *name) {
+	
+	//int y = read(fd, buf, sizeof(buf));
+	//write(1, buf, y);
+
+	int n, m, total_number_of_lines, i;
+	total_number_of_lines = 0;
+
+	while((n = read(fd, buf, sizeof(buf))) > 0) {
+		for(i = 0; i<n; i++){
+			if(buf[i] == '\n')
+				total_number_of_lines++;
+		}
+	}
+	close(fd);
+	fd = open(name, 0);
+
+	int index_of_first_line = total_number_of_lines - 10;
+	int current_line_number;
+	
+	while((m = read(fd, buf, sizeof(buf))) > 0) {
+		for(i = 0; i<m; i++) {
+			if(buf[i] == '\n') {
+				current_line_number++;
+			}
+			if(current_line_number == index_of_first_line) {
+				write(1, &buf[i], m);
+				break;
+			}
+		}
+	}
+		
+}
+
 int main(int argc, char *argv[]) {
 	
 	int fd, x;
-	int y;
 
 	if(argc <= 1) {
 		printf(1, "Must enter file name!\n");
@@ -19,43 +53,9 @@ int main(int argc, char *argv[]) {
 			printf(1, "tail: cannot open %s\n", argv[x]);
 			exit();
 		}
-		fd = open(argv[x], 0);
-		y = read(fd, buf, sizeof(buf));
-		write(1, buf, y); 
+		tail(fd, argv[x]);
+		close(fd);
 	}
 
-
-	/*int n, m, total_number_of_lines, i;
-	total_number_of_lines = 0;
-	int fd = open("README", 0);
-	
-	while((n = read(fd, buf, sizeof(buf))) > 0) { 
-		for(i = 0; i<n; i++){
-			if(buf[i] == '\n')
-				total_number_of_lines++;
-		}
-	}
-	close(fd);
-	fd = open("README", 0);
-
-	int index_of_first_line, current_line_number;
-	index_of_first_line = total_number_of_lines - 10;
-//	printf(1,"Total number of lines: %d\n", total_number_of_lines);
-//	printf(1,"First line we want to print: %d\n", index_of_first_line);
-
-
-	while((m = read(fd, buf, sizeof(buf))) > 0) {
-		for(i = 0; i <m; i++) {
-			if(buf[i] == '\n') {
-				current_line_number++;
-			}
-			if(current_line_number == index_of_first_line) {
-				write(1, &buf[i], m);
-				break;
-			} 
-		}
-	}*/
-
-	close(fd);
 	exit();
 }

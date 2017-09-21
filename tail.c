4cb11488aae797a9ab9efa741c3262d880bfa34c
Carolynn Vu <carolynn.vu@gmail.com>
2017-09-21 16:07:57 -0400

    replace while loops

--- tail.c
+++ tail.c
@@ -6,37 +6,37 @@ char buf[4096];
 
 
 void tail(int fd, char *name) {
-	
-	//int y = read(fd, buf, sizeof(buf));
-	//write(1, buf, y);
 
-	int n, m, total_number_of_lines, i;
+	int n, total_number_of_lines, i;
 	total_number_of_lines = 0;
 
-	while((n = read(fd, buf, sizeof(buf))) > 0) {
+	n = read(fd, buf, sizeof(buf));
+
+	if(n > 0){
 		for(i = 0; i<n; i++){
 			if(buf[i] == '\n')
 				total_number_of_lines++;
 		}
 	}
-	close(fd);
-	fd = open(name, 0);
+
+	if(n < 0) {
+		printf(1, "tail: read error\n");
+		exit();
+	}
 
 	int index_of_first_line = total_number_of_lines - 10;
-	int current_line_number;
+	int current_line_number = 0;
 	
-	while((m = read(fd, buf, sizeof(buf))) > 0) {
-		for(i = 0; i<m; i++) {
-			if(buf[i] == '\n') {
-				current_line_number++;
-			}
-			if(current_line_number == index_of_first_line) {
-				write(1, &buf[i], m);
-				break;
-			}
+	for(i = 0; i<n; i++) {
+		if(buf[i] == '\n') {
+			current_line_number++;
+		}
+		if(current_line_number == index_of_first_line) {
+			write(1, &buf[i], n);
+			break;
 		}
 	}
-		
+	
 }
 
 int main(int argc, char *argv[]) {
@@ -44,7 +44,7 @@ int main(int argc, char *argv[]) {
 	int fd, x;
 
 	if(argc <= 1) {
-		printf(1, "Must enter file name!\n");
+		tail(0, "");
 		exit();
 	}
 

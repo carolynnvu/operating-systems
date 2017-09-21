4a35add4000d44264e961523fe5f6234d9aeb737
Carolynn Vu <carolynn.vu@gmail.com>
2017-09-21 00:08:11 -0400

    update to read file name

--- tail.c
+++ tail.c
@@ -4,9 +4,28 @@
 
 char buf[4096];
 
-int main() {
+int main(int argc, char *argv[]) {
+	
+	int fd, x;
+	int y;
 
-	int n, m, total_number_of_lines, i;
+	if(argc <= 1) {
+		printf(1, "Must enter file name!\n");
+		exit();
+	}
+
+	for(x = 1; x < argc; x++){
+		if((fd = open(argv[x], 0)) < 0){ 
+			printf(1, "tail: cannot open %s\n", argv[x]);
+			exit();
+		}
+		fd = open(argv[x], 0);
+		y = read(fd, buf, sizeof(buf));
+		write(1, buf, y); 
+	}
+
+
+	/*int n, m, total_number_of_lines, i;
 	total_number_of_lines = 0;
 	int fd = open("README", 0);
 	
@@ -21,9 +40,8 @@ int main() {
 
 	int index_of_first_line, current_line_number;
 	index_of_first_line = total_number_of_lines - 10;
-	printf(1,"Total number of lines: %d\n", total_number_of_lines);
-	printf(1,"First line we want to print: %d\n", index_of_first_line);
-	//index -10 to account for 0 start, instead of t - 10 + 1
+//	printf(1,"Total number of lines: %d\n", total_number_of_lines);
+//	printf(1,"First line we want to print: %d\n", index_of_first_line);
 
 
 	while((m = read(fd, buf, sizeof(buf))) > 0) {
@@ -36,8 +54,8 @@ int main() {
 				break;
 			} 
 		}
-	}
+	}*/
 
-
+	close(fd);
 	exit();
 }

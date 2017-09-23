a9790dfb8ca425e0782093b3127c0b359f7d2059
Carolynn Vu <carolynn.vu@gmail.com>
2017-09-23 16:04:45 -0400

    add tail len flag

--- tail.c
+++ tail.c
@@ -2,6 +2,9 @@
 #include "stat.h"
 #include "user.h"
 
+#define DEFAULT_TAIL_LEN 10
+
+int tail_len;
 char buf[4096];
 
 
@@ -23,9 +26,8 @@ void tail(int fd, char *name) {
 		printf(1, "tail: read error\n");
 		exit();
 	}
-	//total_number_of_lines > 10 ? total_number_of_lines - 10 : 0
 
-	int index_of_first_line = total_number_of_lines - 10;
+	int index_of_first_line = ((total_number_of_lines > tail_len) ? (total_number_of_lines - tail_len) : 0); 
 	int current_line_number = 0;
 	
 	for(i = 0; i<n; i++) {
@@ -55,9 +57,7 @@ void tail_stdi(int fd, char *name) {
 	}
 
 	int current_line_number = 0;
-	int index_of_first_line = (total_number_of_lines > 10 ? total_number_of_lines - 10 : 0);
-
-	printf(1, "num lines: %d\n", total_number_of_lines);
+	int index_of_first_line = (total_number_of_lines > DEFAULT_TAIL_LEN ? total_number_of_lines - DEFAULT_TAIL_LEN : 0);
 
 	for(i = 0; i<char_count; i++){
 		if(buf[i] == '\n') {
@@ -70,16 +70,44 @@ void tail_stdi(int fd, char *name) {
 	}
 }
 
+void set_tail_len(char *str) {
+	if(str[0] != '-') 
+		tail_len = DEFAULT_TAIL_LEN;
+	else {
+		char * str_cp = str;
+		str_cp = str_cp + 1;
+		if(str_cp[0] == '\0') {
+			tail_len = DEFAULT_TAIL_LEN;
+		} else { 
+			tail_len = atoi(str_cp);
+		}
+	}
+}
+
+int str_is_tail_len_flag(char *str) {
+	return (str[0] == '-') ? 1 : 0;
+}
+
 int main(int argc, char *argv[]) {
 	
-	int fd, x;
+	int fd, x, start_index;
 
 	if(argc <= 1) {
 		tail_stdi(0, "");
 		exit();
 	}
 
-	for(x = 1; x < argc; x++){
+	char *str_cp = argv[1];
+	if(str_is_tail_len_flag(str_cp)) {
+		start_index = 2;
+		set_tail_len(str_cp);
+	} else {
+		tail_len = DEFAULT_TAIL_LEN;
+		start_index = 1;
+	}
+
+
+	for(x = start_index; x < argc; x++){
 		if((fd = open(argv[x], 0)) < 0){ 
 			printf(1, "tail: cannot open %s\n", argv[x]);
 			exit();
